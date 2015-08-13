b01e2006be49b4359a56be51c9d52b949636837d
ivebencrazy <ben@blanketwarriors.com>
2015-08-13 14:59:45 -0700

    [feature] Adds search functionality for blog posts

--- app/components/Blog.js
+++ app/components/Blog.js
@@ -4,9 +4,65 @@ import React from 'react';
 import Router from 'react-router';
 
 class Blog extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      unfiltered: _.sortBy(props.collections.BlogPosts, function(p) { return p.index; }),
+      filtered: [],
+      search: ''
+    }
+    this.onSearchUpdate = _.bind(this.onSearchUpdate, this);
+    this.onTagUpdate = _.bind(this.onTagUpdate, this);
+    this.resetSearch = _.bind(this.resetSearch, this);
+  }
+
+  // Takes each unfiltered item and returns a boolean for matching search criteria
+  filter(item) {
+    var search = this.state.search;
+    var check = function(toCheck) {
+      if(typeof toCheck === 'string' || typeof toCheck === 'number'){
+        return Boolean(toCheck.match(new RegExp(search, 'ig')));
+      }
+      if(typeof toCheck === 'object') {
+        return _.some(toCheck, function(item) {
+          return check(item);
+        });
+      }
+      return false;
+    };
+    return check([item.title, item.author.name, item.date, item.tags]);
+  }
+
+  // Update functions for filtering
+  onSearchUpdate(event) {
+    this.setState({search: event.target.value}, _.bind(function(){
+      this.setState({
+        filtered: _.filter(this.state.unfiltered, _.bind(this.filter, this))
+      });
+    }, this));
+  }
+
+  onTagUpdate(event) {
+    this.setState({search: event.target.innerText}, _.bind(function(){
+      this.setState({
+        filtered: _.filter(this.state.unfiltered, _.bind(this.filter, this))
+      });
+    }, this));
+  }
+
+  resetSearch() {
+    this.setState({search: ''}, _.bind(function(){
+      this.setState({
+        filtered: _.filter(this.state.unfiltered, _.bind(this.filter, this))
+      });
+    }, this));
+  }
+
   render() {
-    let blogPosts = _.sortBy(this.props.collections.BlogPosts, function(p) {
-      return p.index;
+    let blogPosts = this.state.search ? this.state.filtered : this.state.unfiltered;
+    let tags = _.reduce(this.state.unfiltered, function(total, blogPost) {
+      return _.union(total, blogPost.tags);
     });
 
     let blogPostList = _.map(blogPosts, function(blogPost) {
@@ -14,12 +70,23 @@ class Blog extends React.Component {
         <div className="bigLink" key={blogPost.name}>
           <Router.Link to="blogPost" params={blogPost}>
             <h1 className="item">{blogPost.title}</h1>
+            <ul className="tags">
+              {_.map(blogPost.tags, function(tag) { return <li className="tag" key={tag}>{tag}</li> })}
+            </ul>
           </Router.Link>
         </div>
       );
     });
+    blogPostList = blogPostList.length ? blogPostList : <div>There are no matching blog posts!</div>
 
-    return <div className="list page">{blogPostList}</div>;
+    return (
+      <div className="list blog page">
+        <div className="searchWrapper">
+          <input type="search" placeholder="search..." className="search" value={this.state.search} onChange={this.onSearchUpdate} />
+        </div>
+        <div className="posts">{blogPostList}</div>
+      </div>
+    );
   }
 }
 
